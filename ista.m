268d3ae371d9dee85b0299c2a592b5ac787f9c6a
devansh-dvj <devansh.dvj@gmail.com>
2021-05-27 12:55:12 +0530

    Added comments to ista function

--- ista.m
+++ ista.m
@@ -1,11 +1,38 @@
+
 function theta = ista(y, A, lambda, alpha, iter)
+    % Input:
+    %   y : Signal
+    %   A : Overcomplete dictionary
+    %   lambda : Weight of l1 norm
+    %   alpha : Step size
+    %   iter : Number of iterations
+    % Output:
+    %   theta : Sparse coefficients
+    % Brief:
+    %   Iterative  for solving y = A*theta where theta is sparse
+
+    % Initialize theta
     theta = zeros(size(A, 2), 1);
+
+    % Set soft thershold
     thres = lambda/(2*alpha);
+
+    % Iteratively converge
     for i=1:iter
         theta = soft(theta + (A'*(y - A*theta))/alpha, thres);
     end
 end
 
 function y = soft(x,T)
+    % Input:
+    %   x : Input value
+    %   T : Soft thresholding value (> 0)
+    % Output:
+    %   y : Output value
+    % Brief:
+    %   if x < -T, y = x + T
+    %   if -T < x < T, y = 0
+    %   if x > T, y = x - T
+
     y = sign(x).*(max(0, abs(x)-T));
 end
