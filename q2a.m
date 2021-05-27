f507034b5c49942980957effc0f01fc469f37fad
devansh-dvj <devansh.dvj@gmail.com>
2021-05-27 18:11:07 +0530

    Updated q2a

--- q2a.m
+++ q2a.m
@@ -2,54 +2,74 @@ clc;
 clear;
 close all;
 
-% Setting seed
+%% Set seed
 rng(0);
 
-% Reading
+%% Read the image
 orig = cast(imread("data/barbara256.png"),'double');
+
+%% Constants
+% Set the Height and Width of the image
 H = size(orig, 1);
 W = size(orig, 2);
-% figure; imshow(cast(orig, 'uint8'));
+% Set standard deviation of Gaussian Noise
+noise_std = 2;
+% Set Patch size
+ps = 8;
 
-% Adding Gaussian Noise
-noise_img = orig + 2*randn(H, W);
-% figure; imshow(cast(noise_img, 'uint8'));
+%% Get the noisy image using additive gaussian noise
+noisy = orig + noise_std*randn(H, W);
 
-% Calculating phi, psi and thus, A.
-phi = diag(ones(64,1));
-psi = kron(dctmtx(8)', dctmtx(8)');
-A = phi*psi;
+%% Reconstruction of the original image
+% Define the orthonormal matrix in which the patches are sparse - here, 2D-DCT
+psi = kron(dctmtx(ps)', dctmtx(ps)');
 
-% Setting alpha, lambda and number of iterations
-alpha = floor(eigs(A'*A,1)) + 1;
-lambda = 1;
+% Define the sensing matrix as Identity
+phi = diag(ones(ps*ps, 1));
+
+% Define the sensing matrix w.r.t. DCT coefficients
+A = phi * psi;
+
+% Set alpha, lambda and number of iterations for ISTA
+alpha = floor(eigs(A'*A, 1)) + 2;
+lambda = 3 * noise_std;
 iter = 100;
 
 % Initializing reconstructed image and averaging matrix
-recon_img = zeros(H, W, 'double');
+recon = zeros(H, W, 'double');
 avg_mat = zeros(H, W, 'double');
 
-tic; % Timer start
+tic;
+% For every (overlapping) patch
+for i=1:H-ps+1
+    for j=1:W-ps+1
+        % Get the noisy patch
+        y = phi * reshape(noisy(i:i+ps-1,j:j+ps-1), [ps*ps 1]);
 
-% Iterating over all possible 8x8 patches in the image
-for i=1:H-7
-    for j=1:W-7
-        y = phi * reshape(noise_img(i:i+7,j:j+7), [8*8 1]);
+        % Use ISTA to obtain the DCT coefficients
         theta = ista(y, A, lambda, alpha, iter);
-        recon_img(i:i+7,j:j+7) = recon_img(i:i+7,j:j+7) + reshape(psi * theta, [8 8]);
-        avg_mat(i:i+7,j:j+7) = avg_mat(i:i+7,j:j+7) + ones(8,8);
-        i, j % Prints the coordinates, to check for speed and debugging
+
+        % Update the reconstructed patch from the coefficients
+        recon(i:i+ps-1, j:j+ps-1) = recon(i:i+ps-1, j:j+ps-1) + reshape(psi * theta, [ps ps]);
+        avg_mat(i:i+ps-1, j:j+ps-1) = avg_mat(i:i+ps-1, j:j+ps-1) + ones(ps, ps);
+
+        % Print the co-ordinates of the patch, to check for speed and debugging
+        fprintf('(%i, %i)\n', i, j);
     end
 end
 
-% Normalize the reconstructed image
-recon_img(:,:) = recon_img(:,:)./avg_mat(:,:);
-recon_img(recon_img < 0) = 0;
-recon_img(recon_img > 255) = 255;
+%% Normalize the reconstructed frames
+recon = recon ./ avg_mat;
+recon = cast(recon, 'uint8');
+orig = cast(orig, 'uint8');
 
-% Save the image and calculate RMSE
-figure; imshow(cast([recon_img(:,:), orig(:,:)], 'uint8'));
-imwrite(cast([recon_img(:,:), orig(:,:)], 'uint8'), 'results/q2a.png');
-fprintf('RMSE : %f\n', norm(recon_img(:,:) - orig(:,:), 'fro')^2 / norm(orig(:,:)^2, 'fro'));
+%% Save the result and Compute RMSE (Relative Mean Squared Error)
+% Display and Save the reconstructed frame
+figure; imshow([orig, recon]);
+imwrite(orig, 'results/q2a_orig.png');
+imwrite(recon, 'results/q2a_recon.png');
+% RMSE of the reconstructed image
+fprintf('RMSE : %f\n', (norm(double(recon - orig), 'fro')^2 / norm(double(orig), 'fro')^2));
 
-toc; % Timer end
+% Evaluate the time taken
+toc;
